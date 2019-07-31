8bc476d03a8f8a6e63a0e4ab6c35fd19bdbb87b6
koujr <1062590935@qq.com>
2019-07-31 10:15:58 +0800

    git 第三方登录

--- src/main/java/life/kou/community/controller/AuthorizeController.java
+++ src/main/java/life/kou/community/controller/AuthorizeController.java
@@ -1,14 +1,32 @@
 package life.kou.community.controller;
 
+import life.kou.community.dto.AccessTokenDto;
+import life.kou.community.dto.GithubUser;
+import life.kou.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                             @RequestParam(name = "state") String state){
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setClient_id("b2f8b36b874051126cb4");
+        accessTokenDto.setClient_secret("5f20b145dbfd06f51e9fe77cb907681379f3022e");
+        accessTokenDto.setCode(code);
+        accessTokenDto.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDto.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
+
