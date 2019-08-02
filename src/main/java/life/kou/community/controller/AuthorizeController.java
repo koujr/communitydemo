44973619d75733c8cccbe1e98657b654698f2eb0
koujr <1062590935@qq.com>
2019-08-02 16:20:49 +0800

    获取cookies，判断用户是否存在登录

--- src/main/java/life/kou/community/controller/AuthorizeController.java
+++ src/main/java/life/kou/community/controller/AuthorizeController.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -33,7 +35,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                             @RequestParam(name = "state") String state,
-                            HttpServletRequest request){
+                            HttpServletResponse response){
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clientSecret);
@@ -44,15 +46,18 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getUser(accessToken);
         //如果githubUser不为空，说明登录成功
         if(null != githubUser){
+            //将登陆用户数据插入数据库
             User user =new  User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModify(user.getGmtCreate());
             userMapper.insert(user);
-            //登录成功，写入cookie和session
-            request.getSession().setAttribute("user",githubUser);
+            response.addCookie(new Cookie("token" , token));
+           /* //登录成功，写入cookie和session
+            request.getSession().setAttribute("user",githubUser);*/
             return "redirect:/";
         }else{
             //登录失败，重新登录
--- src/main/java/life/kou/community/controller/IndexController.java
+++ src/main/java/life/kou/community/controller/IndexController.java
@@ -1,14 +1,37 @@
 package life.kou.community.controller;
 
+import life.kou.community.mapper.UserMapper;
+import life.kou.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        //获取cookies
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for (Cookie cookie : cookies) {
+                if("token".equals(cookie.getName())){
+                    String token = cookie.getValue();
+                    //判断用户是否存在登录token
+                    User user = userMapper.findByToken(token);
+                    if(user != null){
+                        request.getSession().setAttribute("user",user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
